2f4add43ee24ad190cf7d233de31802665d6080d
MatheusADSantos <matheusdevmobile2020@gmail.com>
2020-03-04 10:52:42 -0300

    Showing a score in navBar, presenting a alert when trying above 10 times and when you mistake, show why

--- Project2_HackingWithSwift/ViewController.swift
+++ Project2_HackingWithSwift/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var times = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,15 @@ class ViewController: UIViewController {
 //                print(error)
 //            }
 //        }
+//        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(askQuestion))
+//        let play = UIBarButtonItem(title: "Play", style: .plain, target: self, action: #selector(askQuestion))
+        
+        
+        let change = UIBarButtonItem(title: "Change", style: .done, target: self, action: #selector(askQuestion(action:)))
+        navigationItem.leftBarButtonItem = change
+        
+        let scoreTotal = UIBarButtonItem(title: "SCORE: \(score)", style: .plain, target: self, action: #selector(showTimeLineScore(action:)))
+        navigationItem.rightBarButtonItem = scoreTotal
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         button1.layer.borderWidth = 1
@@ -38,18 +48,46 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        askQuestion()
+//        askQuestion()
     }
 
-    func askQuestion(action: UIAlertAction! = nil) {
+    @objc func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         title = countries[correctAnswer].uppercased()
-        print("countries", countries, "correctAnswer", correctAnswer, "title", title)
+        times += 1
+//        print("countries", countries, "correctAnswer", correctAnswer, "title", title)
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        controllerTimes(times: times)
+    }
+    
+    func controllerTimes(times: Int) {
+        print("Times: \(times)")
+        if times >= 10 {
+            let ac = UIAlertController(title: "Final", message: """
+                Congratulations!
+                Your score is \(score)
+                """ ,preferredStyle: .alert)
+            let tryAgain = UIAlertAction(title: "Try again", style: .default) { (action) in
+                self.score = 0
+                self.times = 0
+                self.navigationItem.rightBarButtonItem?.title = "SCORE: \(0)"
+                self.askQuestion(action: nil)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .destructive) { (action) in
+                self.button1.isEnabled = false
+                self.button2.isEnabled = false
+                self.button3.isEnabled = false
+                self.navigationItem.leftBarButtonItem?.isEnabled = false
+            }
+            ac.addAction(tryAgain)
+            ac.addAction(cancel)
+            present(ac, animated: true)
+        }
     }
     
     
@@ -59,14 +97,35 @@ class ViewController: UIViewController {
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
+            askQuestion(action: nil)
         } else {
             title = "Wrong"
             score -= 1
+            let ac = UIAlertController(title: "Atention", message: "Wrong! That’s the flag of \(countries[sender.tag])", preferredStyle: .alert)
+            let aa = UIAlertAction(title: "Try Again!"  , style: .default, handler: askQuestion(action:))
+            ac.addAction(aa)
+            present(ac, animated: true)
+
+        }
+        if let buttonRight = self.navigationItem.rightBarButtonItem {
+            buttonRight.title = "SCORE: \(score)"
+            if self.score >= 0 {
+                buttonRight.tintColor = .blue
+            } else {
+                buttonRight.tintColor = .red
+            }
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
+        
+                
+        
+//        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+//        present(ac, animated: true)
+    }
+    
+    @objc func showTimeLineScore(action : UIBarButtonItem! = nil) {
+        print("showing")
     }
     
 }
